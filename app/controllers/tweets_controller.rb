5984d493ea2a8fbea528996646b54a7e71d71e71
leevic31 <leevic31@gmail.com>
2021-12-21 16:54:54 -0500

    Added date range filter

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,10 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = TrumpTweet.page params[:pages]
+    @tweets = []
+    if params[:start_date] && params[:end_date]
+      start_date = params[:start_date]
+      end_date = params[:end_date]
+      @tweets = TrumpTweet.where(:publishedAt => start_date..end_date).page params[:page]
+    end
   end
 end
--- app/models/trump_tweet.rb
+++ app/models/trump_tweet.rb
@@ -1,3 +1,2 @@
 class TrumpTweet < ActiveRecord::Base
-    paginates_per 4
 end
